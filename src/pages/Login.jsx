4feeef3e56892352add872f2e2809764bf67a0fc
Sujain Shah <shahsujain12@gmail.com>
2026-07-05 20:43:18 +0530

    Day 1: React project setup and Login page UI

---
+++ src/pages/Login.jsx
@@ -0,0 +1,24 @@
+function Login() {
+  return (
+    <div>
+      <h1>AI Code Review Assistant</h1>
+
+      <h2>Login</h2>
+
+      <input type="email" placeholder="Enter your email" />
+
+      <br />
+      <br />
+
+      <input type="password" placeholder="Enter your password" />
+
+      <br />
+      <br />
+
+      <button>Login</button>
+      <p>Don't have an account? Sign Up</p>
+    </div>
+  );
+}
+
+export default Login;
